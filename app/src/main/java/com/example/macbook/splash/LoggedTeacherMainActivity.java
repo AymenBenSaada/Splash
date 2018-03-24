2a97d665a39f757bff31e5f041e6895cbfd91398
nader2560 <nader.zouaoui@edonec.com>
2018-03-24 19:37:14 +0100

    Saturday afternoon

--- app/src/main/java/com/example/macbook/splash/LoggedTeacherMainActivity.java
+++ app/src/main/java/com/example/macbook/splash/LoggedTeacherMainActivity.java
@@ -2,6 +2,7 @@ package com.example.macbook.splash;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.Typeface;
 import android.net.Uri;
@@ -198,9 +199,11 @@ public class LoggedTeacherMainActivity extends AppCompatActivity {
     public void selectItemDrawer(MenuItem menuItem) {
         switch (menuItem.getItemId()) {
             case R.id.logout:
-                //TODO: fix the logout logic here!!
-                Intent i = new Intent(this,CommunSuggestionsActivity.class);
-                startActivity(i);
+                SharedPreferences sharedPreferences = getSharedPreferences("isConnected",MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("AccountType","");
+                editor.putBoolean("isConnected",false);
+                editor.putInt("userId",0);
                 break;
             default:
                 return;
